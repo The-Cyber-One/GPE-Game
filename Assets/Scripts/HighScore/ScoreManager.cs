3094f3213f2631d33725b6189fb611f19adc3ec7
Daangast7 <daanmeijer035@gmail.com>
2023-09-21 14:00:25 +0200

    Functioning Highscore system

---
+++ Assets/Scripts/HighScore/ScoreManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+using System;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI inputScore;
+    [SerializeField]
+    private TMP_InputField inputName;
+
+    public UnityEvent<string, int> sumbitScoreEvent;
+
+    public void SumbitScore()
+    {
+        sumbitScoreEvent.Invoke(inputName.text, int.Parse(removeWord(inputScore.text, "Score:")));
+    }
+
+    public static String removeWord(String str, String word)
+    {
+
+        // Check if the word is present in string
+        // If found, remove it using removeAll()
+        if (str.Contains(word))
+        {
+
+            // To cover the case
+            // if the word is at the
+            // beginning of the string
+            // or anywhere in the middle
+            String tempWord = word + " ";
+            str = str.Replace(tempWord, "");
+
+            // To cover the edge case
+            // if the word is at the
+            // end of the string
+            tempWord = " " + word;
+            str = str.Replace(tempWord, "");
+        }
+
+        // Return the resultant string
+        return str;
+    }
+}
