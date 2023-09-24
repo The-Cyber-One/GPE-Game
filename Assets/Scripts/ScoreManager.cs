3e553a53f8cbfd96cfc541472442a9f533ffe94b
Daangast7 <daanmeijer035@gmail.com>
2023-09-24 15:18:38 +0200

    Added a dropped animation and sound when Island is full and player creates island

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -8,9 +8,11 @@ public class ScoreManager : Singleton<ScoreManager>
     [SerializeField] private float maxMultiplier = 5f, multiplierBonusAmount = 4f;
     [SerializeField] private TMP_Text scoreText, multiplierText, islandScoreText, totalScoreText;
     [SerializeField] private AudioSource bonusAudio;
+    [SerializeField] private AudioSource fullislandAudio;
+    [SerializeField] private Animator animator;
 
     private int _score = 0, _totalScore = 0;
-    private float _multiplier = 0, _bonusMultiplier = 0;
+    private float _multiplier = 0, _bonusMultiplier = 0, _filledPercentage = 0;
 
     private void OnValidate()
     {
@@ -28,6 +30,7 @@ public class ScoreManager : Singleton<ScoreManager>
     {
         _score += amount;
         _multiplier = maxMultiplier * filledPercentage + _bonusMultiplier;
+        _filledPercentage = filledPercentage;
         UpdateUI();
     }
 
@@ -43,6 +46,12 @@ public class ScoreManager : Singleton<ScoreManager>
     [ContextMenu(nameof(SaveIslandScore))]
     public void SaveIslandScore()
     {
+        if (_filledPercentage == 1)
+        {
+            animator.SetTrigger("ShouldPlay");
+            fullislandAudio.Play();
+        }
+        _filledPercentage = 0;
         _totalScore += Mathf.FloorToInt(_score * _multiplier);
         _score = 0; 
         _multiplier = _bonusMultiplier = 0;
